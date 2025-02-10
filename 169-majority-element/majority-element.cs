15c436f27cbf238f267dbed80557f5c5b0eb1323
Arnau Mora Coll <143502251+amoracoll@users.noreply.github.com>
2025-02-10 08:54:51 +0100

    Time: 1 ms (73.30%) | Memory: 49.9 MB (16.14%) - LeetSync

--- 169-majority-element/majority-element.cs
+++ 169-majority-element/majority-element.cs
@@ -1,33 +1,5 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
-        // int finalNumber = 0;
-        // int generalCounter = 0;
-
-        // for (int i = 0; i < nums.Length; i++)
-        // {
-        //     int localCounter = 0;
-
-        //     for (int x = 0; x < nums.Length; x++)
-        //     {
-        //         if (nums[i] != nums[x])
-        //         {
-        //             continue;
-        //         }
-
-        //         localCounter++;
-        //     }
-
-        //     if (generalCounter > localCounter)
-        //     {
-        //         continue;
-        //     }
-
-        //     generalCounter = localCounter;
-        //     finalNumber = nums[i];
-        // }
-
-        // return finalNumber;
-
         int candidate = nums[0];
         int count = 0;
 
